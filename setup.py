5b1f5fdbe0c825755c7f6d0d74d913671bfb3ab7
Arnur <arnursabet@gmail.com>
2020-08-18 15:19:16 -0400

    added liscence, editted pipfile

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='FaceRecognitionSystem',
-      version='1.0',
+setup(name='frsystem',
+      version='1.0.0',
       description='A system for experimenting with face recognition',
       long_description = long_description,
       long_description_content_type = "text/markdown",
@@ -14,6 +14,6 @@ setup(name='FaceRecognitionSystem',
       author_email='arnursabet@gmail.com',
       license='GNU GENERAL PUBLIC LICENSE',
       install_requires=["numpy","requests","mtcnn","scikit_learn","tensorflow>=2.2.0"],
-      packages=['FaceRecognitionSystem'],
+      packages=['frsystem'],
       zip_safe=False)
 
