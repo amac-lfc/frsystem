1622be2aeb3392d9c0989f6483b007b8c36571dc
Arnur <arnursabet@gmail.com>
2020-08-29 16:40:25 -0400

    new version

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 
-with open("README.md", "r") as fh:
+with open("README-frsystem.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='frsystem',
-      version='1.0.0',
+      version='1.1.0',
       description='A system for experimenting with face recognition',
       long_description = long_description,
       long_description_content_type = "text/markdown",
-      url='https://github.com/arnursabet/facialdetection',
+      url='https://github.com/amac-lfc/frsystem',
       author='Arnur Sabet',
       author_email='arnursabet@gmail.com',
       license='GNU GENERAL PUBLIC LICENSE',
-      install_requires=["numpy","requests","mtcnn","scikit_learn","tensorflow>=2.2.0"],
+      install_requires=["numpy","requests","mtcnn","opencv-python","scikit_learn","tensorflow_gpu>=2.2.0","tensorflow-gpu>=2.2.0"],
       packages=['frsystem'],
       zip_safe=False)
 
